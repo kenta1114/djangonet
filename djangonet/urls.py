625224289c9efbcfe20b88dc0cb35fc95a94722b
kenta moriuchi <naturesciense@gmail.com>
2025-08-05 16:51:25 +0900

    add template profile_list.html

--- djangonet/urls.py
+++ djangonet/urls.py
@@ -1,10 +1,11 @@
-# dwitter/urls.py
+# djangonet/urls.py
 
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, profile_list
 
-app_name = "dwitter"
+app_name = "djangonet"
 
 urlpatterns = [
     path("", dashboard, name="dashboard"),
+    path("profile_list/", profile_list, name="profile_list")
 ]
--- social/urls.py
+++ social/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-import djangonet
 
 urlpatterns = [
-    path("", include(djangonet.urls)),
+    path("", include("djangonet.urls")),
     path('admin/', admin.site.urls),
 ]
